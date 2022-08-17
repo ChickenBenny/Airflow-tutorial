d5bc1d01f601f24e16d0ad0803a36f3950587c27
ChickenBenny <zxc123benny14159@gmail.com>
2022-08-17 21:36:00 +0800

    add BashOperation example

---
+++ dags/BashOperator.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+from sched import scheduler
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'Benny',
+    'retries': 5,
+    'retries_delay': timedelta(minutes = 2)
+}
+
+with DAG(
+    dag_id = 'bash_Example',
+    default_args = default_args,
+    description = 'First bash example dag.',
+    start_date = datetime(2022, 8, 16),
+    schedule_interval = '@daily'
+) as dag:
+    task1 = BashOperator(
+        task_id = 'task1',
+        bash_command = "echo hello airflow"
+    )
+
+    task2 = BashOperator(
+        task_id = 'task2',
+        bash_command = "echo task 2 after task 1"
+    )
+    task3 = BashOperator(
+        task_id = 'task3',
+        bash_command = "echo task 3 after task 1"
+    )
+    task4 = BashOperator(
+        task_id = 'task4',
+        bash_command = "echo task 2 after task 1"
+    )
+
+
+    task1 >> [task2, task3]
+    [task2, task3] >> task4
